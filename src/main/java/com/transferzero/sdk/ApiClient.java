4b5fecbad57181ccf4355f896c1dc66f7670ca03
Travis CI <>
2020-03-11 10:33:07 +0000

    BTA-3102 Update SDKs version to 1.6.0. Also update (#175)
    BTA-3102: Release SDKs version 1.6.0 after adding support for BTC transactions.
    
    Changes
    -------
    
    Release SDKs version 1.6.0 after adding support for BTC transactions. Also update bitpesa-api-specification project ruby version to 2.6.5
    
    Risk and impact assessment
    --------------------------
    
    The following PR contains (please tick anything that applies):
    
    * [x] Version upgrades to the code generator
    * [ ] Adding new models to the specification
    * [ ] Contains a major change (see below)
    * [ ] Changing existing models in the specification: [please specify]
    * [ ] Required additional steps required during release: [please specify]
    * [ ] Deemed high risk or impact for other reasons: [please specify]
    
    Please note that any major change in the existing models will break existing SDKs and should be avoided. Major changes include:
    
    * [ ] Any deletion of existing models, including auto-generated models
    * [ ] Any renaming of existing models, including auto-generated models
    * [ ] Any moving of existing models to a different place
    * [ ] Any removal of existing attributes
    * [ ] Any rename of existing attributes
    
    Any major change in the above requires an architectural approval

--- src/main/java/com/transferzero/sdk/ApiClient.java
+++ src/main/java/com/transferzero/sdk/ApiClient.java
@@ -103,7 +103,7 @@ public class ApiClient {
         json = new JSON();
 
         // Set default User-Agent.
-        setUserAgent("TransferZero-SDK/Java7/1.5.0-SNAPSHOT");
+        setUserAgent("TransferZero-SDK/Java7/1.6.0-SNAPSHOT");
     }
 
     /**
